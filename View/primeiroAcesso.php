6c51553c1ee862d88a792ea7fdc2011e68013352
M.Eduarda <mcdudavk@gmail.com>
2026-05-08 20:19:04 -0300

    parte primeiro acesso, criada

---
+++ View/primeiroAcesso.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+<meta charset="UTF-8">
+
+<link rel="stylesheet"
+href="https://www.w3schools.com/w3css/4/w3.css">
+
+<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<title>Primeiro Acesso</title>
+
+</head>
+
+<body>
+
+<form action="" method="post"
+class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin w3-display-middle"
+style="width:30%;">
+
+<h2 class="w3-center">Primeiro Acesso</h2>
+
+<input class="w3-input w3-border w3-round-large w3-margin"
+name="txtNome"
+type="text"
+placeholder="Nome Completo">
+
+<input class="w3-input w3-border w3-round-large w3-margin"
+name="txtCPF"
+type="text"
+placeholder="CPF">
+
+<input class="w3-input w3-border w3-round-large w3-margin"
+name="txtEmail"
+type="text"
+placeholder="Email">
+
+<input class="w3-input w3-border w3-round-large w3-margin"
+name="txtSenha"
+type="password"
+placeholder="Senha">
+
+<button name="btnCadastrar"
+class="w3-button w3-blue w3-block w3-round-large">
+Cadastrar
+</button>
+
+</form>
+
+</body>
+</html>
