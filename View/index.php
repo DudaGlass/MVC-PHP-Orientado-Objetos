8d0d3e226b4eac74e2fb22b9b4f16c616655fc3e
M.Eduarda <mcdudavk@gmail.com>
2026-05-08 20:14:20 -0300

    projetom praticamente pronto

---
+++ View/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+
+    <title>Outras Formações</title>
+
+    <!-- W3CSS -->
+    <link rel="stylesheet"
+          href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body class="w3-light-grey">
+
+    <!-- DIV PRINCIPAL -->
+    <div class="w3-padding-64 w3-content">
+
+        <!-- TÍTULO -->
+        <h2 class="w3-text-cyan">
+            Outras Formações
+        </h2>
+
+        <!-- FORMULÁRIO -->
+        <form action=""
+              method="post"
+              class="w3-row w3-light-grey w3-text-blue w3-margin"
+              style="width:70%;">
+
+            <!-- TEXTOS -->
+            <div class="w3-row w3-center">
+
+                <div class="w3-col"
+                     style="width:50%;">
+
+                    Data Inicial
+
+                </div>
+
+                <div class="w3-rest">
+
+                    Data Final
+
+                </div>
+
+            </div>
+
+            <!-- DATAS -->
+            <div class="w3-row w3-section">
+
+                <!-- DATA INICIAL -->
+                <div class="w3-row w3-section w3-col"
+                     style="width:45%;">
+
+                    <div class="w3-col"
+                         style="width:15%;">
+
+                        <i class="w3-xxlarge fa fa-calendar"></i>
+
+                    </div>
+
+                    <div class="w3-rest">
+
+                        <input class="w3-input w3-border w3-round-large"
+                               name="txtInicioOF"
+                               type="date">
+
+                    </div>
+
+                </div>
+
+                <!-- DATA FINAL -->
+                <div class="w3-row w3-section w3-rest">
+
+                    <div class="w3-col w3-margin-left"
+                         style="width:13%;">
+
+                        <i class="w3-xxlarge fa fa-calendar"></i>
+
+                    </div>
+
+                    <div class="w3-rest">
+
+                        <input class="w3-input w3-border w3-round-large"
+                               name="txtFimOF"
+                               type="date">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- DESCRIÇÃO -->
+            <div class="w3-row w3-section">
+
+                <div class="w3-col"
+                     style="width:7%;">
+
+                    <i class="w3-xxlarge fa fa-align-justify"></i>
+
+                </div>
+
+                <div class="w3-rest">
+
+                    <input class="w3-input w3-border w3-round-large"
+                           name="txtDescEP"
+                           type="text"
+                           placeholder="Ex.: Curso de Inglês - Inglês City">
+
+                </div>
+
+            </div>
+
+            <!-- BOTÃO -->
+            <div class="w3-row w3-section">
+
+                <div class="w3-center">
+
+                    <button name="btnAddOF"
+                            class="w3-button w3-blue w3-round-large"
+                            style="width:20%;">
+
+                        <i class="fa fa-user-plus"></i>
+
+                    </button>
+
+                </div>
+
+            </div>
+
+        </form>
+
+        <!-- TABELA -->
+        <div class="w3-container">
+
+            <table class="w3-table-all w3-centered">
+
+                <thead>
+
+                    <tr class="w3-blue">
+
+                        <th>Início</th>
+                        <th>Fim</th>
+                        <th>Descrição</th>
+                        <th>Apagar</th>
+
+                    </tr>
+
+                </thead>
+
+            </table>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
